f3fd4ce92e2e6b3d376232ca3cf238b7798a83a3
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-20 15:34:33 +0200

    Added js to next button

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log('JS OK')
+
+// Elementi dal DOM
+const buttonNext = document.getElementById('next')
+const buttonPrev = document.getElementById('prev')
+const images = document.querySelectorAll('img')
+
+console.log(images)
+
+
+// Creamo la classe active
+
+let currentActiveIndex = 0;
+images[currentActiveIndex].classList.add('active');
+
+// Eventi Dinamici
+
+// Next Button
+
+buttonNext.addEventListener('click', function(){
+    images[currentActiveIndex].classList.remove('active');
+})
